a2c6418fb503e559d82df4027c5258339321d5cc
SilverStarCat <120988820+SilverStarCat@users.noreply.github.com>
2023-01-11 18:24:42 -0500

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,9 +1,5 @@
 { pkgs }: {
   deps = [
-    pkgs.yarn add moment             # Yarn
-    pkgs.Install-Package Moment.js   # NuGet
-    pkgs.yarn add moment             # Yarn
-    pkgs.yarn add moment             # YarnInstall-Package Moment.js   # NuGetspm install moment --save   # spmmeteor add momentjs:moment  # meteorbower install moment --save # bower (deprecated)npm install moment --save   # npm
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
   ];
